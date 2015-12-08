f41fef50d215837532cb974878338daff5cb0beb
itcocoa <3216730556@qq.com>
2015-12-08 16:54:20 +0800

    增加认证功能，并且只接受POST请求

--- zhifeng/Lib/Action/ServiceAction.class.php
+++ zhifeng/Lib/Action/ServiceAction.class.php
@@ -1,7 +1,40 @@
 <?php
-class ServiceAction  extends Action{
+class ServiceAction  extends Action
+{
+	protected function _initialize()
+	{
+		
+		// 每一个服务方法都执行的内容
+		
+	    // 只接爱POST
+	    if (!IS_POST){
+	        exit ('你没有权限访问！');
+	    }
+	    
+	    // 认证
+	    $this->login($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW']);
+		
+	}
 	
-	protected function send($type,$data) {
+	private function auth()
+	{
+	    header('WWW-Authenticate: Basic realm="My Realm"');
+	    header('HTTP/1.0 401 Unauthorized');
+	    echo '你没有权限访问！'; //Text to send if user hits Cancel button
+	    exit;
+	}
+	
+	private function login($u,$p)
+	{
+	    if ($u != 'admin' || $p != 'adminpass') {
+	        $this->auth();
+	    } else {
+	        //echo $_SERVER['PHP_AUTH_USER'].PHP_EOL.$p;
+	    }
+	}
+	
+	protected function send($type,$data) 
+	{
 	    $r = array(
 	        'type'=>$type,
 	        'data'=>$data
